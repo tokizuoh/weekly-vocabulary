606e62c40d00a2f31d95363f06235326a53ddd78
tokizuoh <tokizuoh@gmail.com>
2024-01-28 15:17:12 +0900

    feat: Hello world!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let lister = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(lister, app).await.unwrap();
 }
