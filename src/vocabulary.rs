38d14be44a8e1b495acf204253ae1fd2a4ff45ad
tokizuoh <tokizuoh@gmail.com>
2024-01-28 16:23:50 +0900

    feat: PartOfSpeech

--- src/vocabulary.rs
+++ src/vocabulary.rs
@@ -1,4 +1,16 @@
+pub enum PartOfSpeech {
+    Noun,
+    Pronoun,
+    Adjective,
+    Verb,
+    Adverb,
+    Preposition,
+    Conjunction,
+    Interjection,
+}
+
 pub struct Vocabulary {
+    pub part_of_speech: PartOfSpeech,
     pub spelling: String,
     pub meaning: String,
 }
@@ -6,6 +18,7 @@ pub struct Vocabulary {
 pub async fn get_latest_vocabulary() -> Vocabulary {
     // TODO: get from DB
     Vocabulary {
+        part_of_speech: PartOfSpeech::Verb,
         spelling: "transcribe".to_string(),
         meaning: "to write down something ecactly as it was said".to_string(),
     }
