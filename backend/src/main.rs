e21c03a0af306ff0ec04c5be11cba3f8392299ef
tokizuoh <tokizuoh@gmail.com>
2024-01-28 17:51:22 +0900

    rename: endpoint

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,11 +7,11 @@ use axum::{
 async fn main() {
     let app = Router::new()
         .route(
-            "/get/vocabulary/recent",
+            "/get/recent",
             get(|| weekly_vocabulary::vocabulary::get_latest_vocabulary()),
         )
-        .route("/get/vocabulary/all", post("TODO"))
-        .route("/register/vocaburary", post("TODO"));
+        .route("/get/all", post("TODO"))
+        .route("/register", post("TODO"))
     let lister = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(lister, app).await.unwrap();
 }
