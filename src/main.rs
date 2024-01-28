eea17c3f399038a489a892cb41466aca1dbd2432
tokizuoh <tokizuoh@gmail.com>
2024-01-28 16:18:04 +0900

    add: routing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| weekly_vocabulary::run()));
+    let app = Router::new()
+        .route("/get/vocabulary/recent", get(|| weekly_vocabulary::run()))
+        .route("/get/vocabulary/all", post("TODO"))
+        .route("/register/vocaburary", post("TODO"));
     let lister = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(lister, app).await.unwrap();
 }
