49a1dd0d61ce7b392282d43dbacf4a7f10e3ed4e
tokizuoh <tokizuoh@gmail.com>
2024-02-04 13:22:55 +0900

    comment

--- backend/src/bin/run_server.rs
+++ backend/src/bin/run_server.rs
@@ -2,6 +2,8 @@ use weekly_vocabulary::route::create_router;
 
 #[tokio::main]
 async fn main() {
+    // TODO: set db and reuse connection pooling
+
     let app = create_router();
     let lister = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(lister, app).await.unwrap();
