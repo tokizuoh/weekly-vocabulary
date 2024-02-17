ccb66597bedcaa7edea53a9473c49aceb5a50855
tokizuoh <tokizuoh@gmail.com>
2024-02-17 11:21:27 +0900

    feat: add tracing for dev

--- backend/application/api/src/bin/run_server.rs
+++ backend/application/api/src/bin/run_server.rs
@@ -1,10 +1,14 @@
 use api::controller;
 use dotenv;
 use mysql::Pool;
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
+
+    tracing_subscriber::fmt::init();
 
     let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = match Pool::new(url.as_str()) {
@@ -16,7 +20,9 @@ async fn main() {
     };
 
     let api = controller::Api { db: pool };
-    let router = generated::server::new(api);
+    let router =
+        generated::server::new(api).layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
+
     let lister = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(lister, router).await.unwrap();
 }
